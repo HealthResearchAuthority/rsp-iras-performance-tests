28b2951828884e08ff1f2e8638cec6ce3411f323
Chris McNeill <chris.mcneill_pa@hra.nhs.uk>
2024-04-08 09:30:56 +0100

    initial commit with 2 perf POC test scripts

---
+++ src/scripts/pocApiScript.js
@@ -0,0 +1,132 @@
+import http from 'k6/http';
+import { check, group, sleep } from 'k6';
+import { Trend } from 'k6/metrics';
+import { randomItem } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
+
+const baseURL = 'https://petstore.swagger.io/v2'
+
+//Request Params to Select From
+const petNames = ['Percy', 'Penelope', 'Polly', 'Paul', 'Phil'];
+const petCategories = ['Pig', 'Python', 'Parrot', 'Porcupine', 'Porpoise'];
+
+export const options = {
+    cloud: {
+        distribution: {
+            'amazon:gb:london': { loadZone: 'amazon:gb:london', percent: 100 },
+        },
+    },
+    scenarios: {
+        PocK6ApiJourney: {
+            executor: 'ramping-vus',
+            gracefulStop: '10s',
+            stages: [
+                { duration: '10s', target: 2 },
+                { duration: '20s', target: 2 },
+                { duration: '10s', target: 0 }
+            ],
+            gracefulRampDown: '10s',
+            exec: 'pocK6ApiJourney',
+        }
+    },
+    thresholds: {
+        http_req_failed: ['rate<0.001'],
+        http_req_duration: ['p(95)<1000', 'p(100)<5000'],
+        //Use Custom Metric As Threshold
+        'delete_pet_response_time': ['p(100)<5000', 'p(95)<1000'],
+
+    },
+};
+
+//Custom Metric
+export const TrendPostPetReqDuration = new Trend('post_pet_response_time', true);
+export const TrendGetPetReqDuration = new Trend('get_pet_response_time', true);
+export const TrendDeletePetReqDuration = new Trend('delete_pet_response_time', true);
+
+export function pocK6ApiJourney() {
+    let response;
+
+    function userThinkTime() {
+        sleep(Math.random() * 2 + 2);
+    };
+
+    function generateRandomId() {
+        const id = Math.floor(100000 + Math.random() * 900000);
+        return id;
+    };
+
+    const postHeaders = {
+        headers: {
+            'Content-Type': "application/json",
+        },
+    };
+
+    group('Create, Fetch & Remove Pet Journey', function () {
+        const petId = generateRandomId();
+        const petCategoryId = generateRandomId()
+        const petCategory = randomItem(petCategories)
+        const petName = randomItem(petNames)
+
+        const postData = JSON.stringify({
+            "id": petId,
+            "category": {
+                "id": petCategoryId,
+                "name": `${petCategory}`
+            },
+            "name": `${petName}`,
+            "photoUrls": [],
+            "tags": [],
+            "status": "available"
+        });
+
+        response = http.post(`${baseURL}/pet`, postData, postHeaders);
+        TrendPostPetReqDuration.add(response.timings.duration);
+        const isPostPetReqSuccessful = check(response,
+            {
+                'Post Pet Request Success': () => response.status === 200,
+                'Post Pet Request Time < 5secs': () => response.timings.duration < 5000,
+                'Post Pet Request Time < 1secs': () => response.timings.duration < 1000
+            });
+        console.log("Request Sent: " + response.request.method + ' ' + response.url, + '\n' + response.request.body);
+        if (!isPostPetReqSuccessful) {
+            console.error(`Post Pet Request Failed - ${response.url} \nStatus - ${response.status}` +
+                `\nResponse Time - ${response.timings.duration} \nError Code - ${response.error_code}`);
+        };
+        userThinkTime();
+
+        response = http.get(`${baseURL}/pet/${petId}`);
+        TrendGetPetReqDuration.add(response.timings.duration);
+        const isGetPetReqSuccessful = check(response,
+            {
+                'Get Pet Request Success': () => response.status === 200,
+                'Get Pet Request Expected Pet Id Returned': (r) => r.body.includes(`"id":${petId}`) == true,
+                'Get Pet Request Time < 5secs': () => response.timings.duration < 5000,
+                'Get Pet Request Time < 1secs': () => response.timings.duration < 1000
+            });
+        console.log("Request Sent: " + response.request.method + ' ' + response.url);
+        if (!isGetPetReqSuccessful) {
+            console.error(`Get Pet Request Failed - ${response.url} \nStatus - ${response.status}` +
+                `\nResponse Time - ${response.timings.duration} \nError Code - ${response.error_code} \nResponse Body - ${response.body}`);
+        };
+        userThinkTime();
+
+        response = http.del(`${baseURL}/pet/${petId}`);
+        TrendDeletePetReqDuration.add(response.timings.duration);
+        const isDeletePetReqSuccessful = check(response,
+            {
+                'Delete Pet Request Success': () => response.status === 200,
+                'Delete Pet Request Expected Pet Id Returned': (r) => r.body.includes(`"message":"${petId}"`) == true,
+                'Delete Pet Request Time < 5secs': () => response.timings.duration < 5000,
+                'Delete Pet Request Time < 1secs': () => response.timings.duration < 1000
+            });
+        console.log("Request Sent: " + response.request.method + ' ' + response.url);
+        if (!isDeletePetReqSuccessful) {
+            console.error(`Delete Pet Request Failed - ${response.url} \nStatus - ${response.status}` +
+                `\nResponse Time - ${response.timings.duration} \nError Code - ${response.error_code} \nResponse Body - ${response.body}`);
+        };
+        userThinkTime();
+    });
+    sleep(1)
+};
+//run both in cloud in morning
+//document k6 proceess
+//draft AC for k6 and PM Pipeline tix, use PW tick as example
---
+++ src/scripts/pocUiScript.js
@@ -0,0 +1,170 @@
+import http from 'k6/http';
+import { check, group, sleep } from 'k6';
+import { Trend } from 'k6/metrics';
+
+const baseURL = 'https://test.k6.io'
+
+export const options = {
+  scenarios: {
+    PocK6UiJourney: {
+      executor: 'ramping-vus',
+      gracefulStop: '10s',
+      stages: [
+        { duration: '10s', target: 2 },
+        { duration: '20s', target: 2 },
+        { duration: '10s', target: 0 },
+      ],
+      gracefulRampDown: '10s',
+      exec: 'pocK6UiJourney',
+    }
+  },
+  thresholds: {
+    http_req_failed: ['rate<0.001'],
+    http_req_duration: ['p(95)<1000', 'p(100)<5000'],
+  },
+};
+
+//Custom Metric
+export const TrendHomePageReqDuration = new Trend('load_k6_home_page_response_time', true);
+export const TrendContactPageReqDuration = new Trend('load_k6_contact_page_response_time', true);
+export const TrendNewsPageReqDuration = new Trend('load_k6_news_page_response_time', true);
+export const TrendMessagesPageReqDuration = new Trend('load_k6_msg_page_response_time', true);
+export const TrendMessagesLoginReqDuration = new Trend('login_k6_msg_page_response_time', true);
+
+export function pocK6UiJourney() {
+  let response;
+
+  function userThinkTime() {
+    sleep(Math.random() * 2 + 2);
+  };
+
+  group('Load k6 Contact Page User Journey', function () {
+    response = http.get(`${baseURL}`);
+    TrendHomePageReqDuration.add(response.timings.duration);
+    const isHomePageReqSuccessful = check(response,
+      {
+        'Home Page Request Success': () => response.status === 200,
+        'Home Page Subtitle Present': (r) => r.body.indexOf('Collection of simple web-pages suitable for load testing.') !== -1,
+        'Home Page Request Time < 5secs': () => response.timings.duration < 5000,
+        'Home Page Request Time < 1secs': () => response.timings.duration < 1000
+      });
+    console.log("Request Sent: " + response.request.method + ' ' + response.url);
+    if (!isHomePageReqSuccessful) {
+      console.error(`Load k6 Home Page Request Failed - ${response.url} \nStatus - ${response.status}` +
+        `\nResponse Time - ${response.timings.duration} \nError Code - ${response.error_code}`);
+    };
+    userThinkTime();
+
+    response = http.get(`${baseURL}/contacts.php`);
+    TrendContactPageReqDuration.add(response.timings.duration);
+    const isContactPageReqSuccessful = check(response,
+      {
+        'Contact Page Request Success': () => response.status === 200,
+        'Contact Page Header Present': (r) => r.body.indexOf('Contact us') !== -1,
+        'Contact Page Request Time < 5secs': () => response.timings.duration < 5000,
+        'Contact Page Request Time < 1secs': () => response.timings.duration < 1000
+      });
+    console.log("Request Sent: " + response.request.method + ' ' + response.url);
+    if (!isContactPageReqSuccessful) {
+      console.error(`Load k6 Contact Page Request Failed - ${response.url} \nStatus - ${response.status}` +
+        `\nResponse Time - ${response.timings.duration} \nError Code - ${response.error_code}`);
+    };
+    userThinkTime();
+  });
+
+  group('Load k6 News Page User Journey', function () {
+    response = http.get(`${baseURL}`);
+    TrendHomePageReqDuration.add(response.timings.duration);
+    const isHomePageReqSuccessful = check(response,
+      {
+        'Home Page Request Success': () => response.status === 200,
+        'Home Page Subtitle Present': (r) => r.body.indexOf('Collection of simple web-pages suitable for load testing.') !== -1,
+        'Home Page Request Time < 5secs': () => response.timings.duration < 5000,
+        'Home Page Request Time < 1secs': () => response.timings.duration < 1000
+      });
+    console.log("Request Sent: " + response.request.method + ' ' + response.url);
+    if (!isHomePageReqSuccessful) {
+      console.error(`Load k6 Home Page Request Failed - ${response.url} \nStatus - ${response.status}` +
+        `\nResponse Time - ${response.timings.duration} \nError Code - ${response.error_code}`);
+    };
+    userThinkTime();
+
+    response = http.get(`${baseURL}/news.php`);
+    TrendNewsPageReqDuration.add(response.timings.duration);
+    const isNewsPageReqSuccessful = check(response,
+      {
+        'News Page Request Success': () => response.status === 200,
+        'News Page Header Present': (r) => r.body.indexOf('In the news') !== -1,
+        'News Page Request Time < 5secs': () => response.timings.duration < 5000,
+        'News Page Request Time < 1secs': () => response.timings.duration < 1000
+      });
+    console.log("Request Sent: " + response.request.method + ' ' + response.url);
+    if (!isNewsPageReqSuccessful) {
+      console.error(`Load k6 News Page Request Failed - ${response.url} \nStatus - ${response.status}` +
+        `\nResponse Time - ${response.timings.duration} \nError Code - ${response.error_code}`);
+    };
+  });
+
+  group('Load k6 and Login to Message', function () {
+    response = http.get(`${baseURL}`);
+    TrendHomePageReqDuration.add(response.timings.duration);
+    const isHomePageReqSuccessful = check(response,
+      {
+        'Home Page Request Success': () => response.status === 200,
+        'Home Page Subtitle Present': (r) => r.body.indexOf('Collection of simple web-pages suitable for load testing.') !== -1,
+        'Home Page Request Time < 5secs': () => response.timings.duration < 5000,
+        'Home Page Request Time < 1secs': () => response.timings.duration < 1000
+      });
+    console.log("Request Sent: " + response.request.method + ' ' + response.url);
+    if (!isHomePageReqSuccessful) {
+      console.error(`Load k6 Home Page Request Failed - ${response.url} \nStatus - ${response.status}` +
+        `\nResponse Time - ${response.timings.duration} \nError Code - ${response.error_code}`);
+    };
+    userThinkTime();
+
+    response = http.get(`${baseURL}/my_messages.php`);
+    TrendMessagesPageReqDuration.add(response.timings.duration);
+    const isMessagesPageReqSuccessful = check(response,
+      {
+        'Unauthorized Messages Page Request Success': () => response.status === 200,
+        'Unauthorized Messages Page Request Time < 5secs': () => response.timings.duration < 5000,
+        'Unauthorized Messages Page Request Time < 1secs': () => response.timings.duration < 1000
+      });
+    console.log("Request Sent: " + response.request.method + ' ' + response.url);
+    if (!isMessagesPageReqSuccessful) {
+      console.error(`Load k6 Unauthorized Messages Page Request Failed - ${response.url} \nStatus - ${response.status}` +
+        `\nResponse Time - ${response.timings.duration} \nError Code - ${response.error_code}`);
+    };
+
+    //Only execute login execution if 
+    const isMessagePageUnauthorized = check(response,
+      {
+        'Unauthorized Messages Header Present': (r) => r.body.indexOf('Unauthorized') !== -1
+      })
+    if (!isMessagePageUnauthorized) {
+      console.error(`Messages Page is not Unauthorized \nNot Attempting Login`);
+    } else {
+      const token = response.html().find('input[name=csrftoken]').first().attr('value');
+      response = http.post(`${baseURL}/login.php`,
+        { login: 'admin', password: '123', redir: '1', csrftoken: `${token}` });
+      TrendMessagesLoginReqDuration.add(response.timings.duration);
+      const isMessageLoginReqSuccessful = check(response,
+        {
+          'Login Messages Request Success': () => response.status === 200,
+          'Authorized Messages Header Present': (r) => r.body.indexOf('Welcome, admin!') !== -1,
+          'Login Messages Page Request Time < 5secs': () => response.timings.duration < 5000,
+          'Login Messages Page Request Time < 1secs': () => response.timings.duration < 1000
+        });
+      console.log("Request Sent: " + response.request.method + ' ' + response.url);
+      if (!isMessageLoginReqSuccessful) {
+        console.error(`Load k6 Authorized Messages Page Request Failed - ${response.url} \nStatus - ${response.status}` +
+          `\nResponse Time - ${response.timings.duration} \nError Code - ${response.error_code}`);
+      };
+    }
+  });
+  sleep(1)
+};
+//do for pet API,
+//group of: post get delete
+//set up account run in cloud
+//document
