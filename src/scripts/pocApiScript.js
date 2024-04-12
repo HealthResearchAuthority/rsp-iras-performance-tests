c6a43d4d087178543559fb9335b97f6122476f47
Chris McNeill <chris.mcneill_pa@hra.nhs.uk>
2024-04-12 16:08:44 +0100

    adjusting filepath of report output & adjusting gitignore

--- src/scripts/pocApiScript.js
+++ src/scripts/pocApiScript.js
@@ -132,6 +132,6 @@ export function pocK6ApiJourney() {
 export function handleSummary(data) {
     return {
         stdout: textSummary(data, { indent: '→', enableColors: true }),
-        'results/report.json': JSON.stringify(data)
-};
+        'tests/results/pocApiReport.json': JSON.stringify(data);
+    };
 }
