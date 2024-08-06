7470c060c95e8ac1aa9ed123e3c6af92bd1874f4
Chris McNeill <chris.mcneill_pa@hra.nhs.uk>
2024-08-06 09:54:36 +0100

    renaming encodedData variable to decodedData

--- src/utils/decryptData.js
+++ src/utils/decryptData.js
@@ -7,25 +7,25 @@ const keyValues = new SharedArray("keyValues", function () {
 });
 
 export async function decryptData(textToDecrypt) {
-  const encodedData = base64Decode(textToDecrypt);
+  const decodedData = base64Decode(textToDecrypt);
   //Key must match the one used to encrypt
   const key = new Uint8Array(keyValues);
 
   try {
-    return await decrypt(key, encodedData);
+    return await decrypt(key, decodedData);
   } catch (e) {
     console.log("Error: " + JSON.stringify(e));
   }
 }
 
-const decrypt = async (key, encodedData) => {
+const decrypt = async (key, decodedData) => {
   // Extract Initialization Vector
   const initializeVectorLength = 12;
-  const iv = new Uint8Array(encodedData.subarray(0, initializeVectorLength));
+  const iv = new Uint8Array(decodedData.subarray(0, initializeVectorLength));
 
   // Extract Encrypted Data
   const encryptedData = new Uint8Array(
-    encodedData.subarray(initializeVectorLength)
+    decodedData.subarray(initializeVectorLength)
   );
 
   // Import Key must match the one used to encrypt
